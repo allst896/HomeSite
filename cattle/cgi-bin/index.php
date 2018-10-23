93983ad79c947a93e57abf8cebbcb5cc82ec7aa5
allst896 <mandi.howell@gmail.com>
2018-10-22 20:54:24 -0500

    Web cattle initial commit

---
+++ cattle/cgi-bin/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cattle</title>
+    </head>
+    <body>
+        <div class="container" id="main-content">
+            <h2>Welcome to our Cattle!</h2>
+        </div>
+        
+        <?php include("includes/load_default.php");?>
+    </body>
+</html>
