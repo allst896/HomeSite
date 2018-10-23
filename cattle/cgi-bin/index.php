2b357026c62fea86fe0c725d2fd1c617d597c183
allst896 <mandi.howell@gmail.com>
2018-10-22 21:21:38 -0500

    Added temp pic

--- cattle/cgi-bin/index.php
+++ cattle/cgi-bin/index.php
@@ -6,8 +6,12 @@
     <body>
         <div class="container" id="main-content">
             <h2>Welcome to our Cattle!</h2>
+            <h3>This is Donkey not one of our cows.</h3>
         </div>
-        
-        <?php include("includes/load_default.php");?>
+
+        <div class="pic-container">
+          <img src="pics/donkey.jpg" alt="not a cow">
+        </div>
+        <!-- <?php include("includes/load_default.php");?> -->
     </body>
 </html>
