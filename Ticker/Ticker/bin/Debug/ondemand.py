118880763b29e7eb76ac13e8868c177f372d88ba
allst896 <mandi.howell@gmail.com>
2018-12-16 12:13:01 -0600

    Removed RanchApp and Ticker from portfolio

--- Ticker/Ticker/bin/Debug/ondemand.py
+++
@@ -1,6 +0,0 @@
-from suds.client import Client
-ondemand = Client('https://marketdata.websol.barchart.com/service?wsdl')
-
-def GetQuote():
-    result = client.service.getQuote('7dc15e2195a9ccc280f286b10584c9c5', 'AAPL')
-    return result
