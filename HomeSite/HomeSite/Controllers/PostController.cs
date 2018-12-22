266595a4d0853e5372916e4e0c6b00277c2fc1b7
allst896 <mandi.howell@gmail.com>
2018-12-22 06:11:33 -0600

    Post page working

---
+++ HomeSite/HomeSite/Controllers/PostController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeSite.Models;
+
+namespace HomeSite.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            var model =
+                from p in _posts
+                orderby p.postDate descending
+                select p;
+
+            return View(model);
+        }
+
+        static List<Post> _posts = new List<Post>
+        {
+            new Post
+            {
+                Id = 1,
+                Title = "First Post",
+                postDate = DateTime.Now,
+                Content = "This is the first post."
+            }
+        };
+    }
+}
---
+++ HomeSite/HomeSite/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeSite.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime postDate { get; set; }
+        public string Content { get; set; }
+        public DateTime editDate { get; set; }
+    }
+}
