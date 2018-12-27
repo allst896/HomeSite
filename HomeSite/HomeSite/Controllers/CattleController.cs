77015d579492573e415da6b3d726c7fba4485e7e
allst896 <mandi.howell@gmail.com>
2018-12-26 19:24:28 -0600

    nested links for Cattle; pics on Cattle/Index

---
+++ HomeSite/HomeSite/Controllers/CattleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeSite.Controllers
+{
+    public class CattleController : Controller
+    {
+        // GET: Cattle
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cows()
+        {
+            return View("Cows");
+        }
+
+        public ActionResult Bulls()
+        {
+            return View("Bulls");
+        }
+    }
+}
