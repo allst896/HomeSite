56664d8457154a83f8a44900e71bcccee044b58e
allst896 <mandi.howell@gmail.com>
2019-01-02 19:04:27 -0600

    Cattle page shows all cattle; working on autocomplete
    autocomplete issues stem from nested Layouts...I think

--- HomeSite/HomeSite/Controllers/CattleController.cs
+++ HomeSite/HomeSite/Controllers/CattleController.cs
@@ -3,15 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HomeSite.Models;
+using PagedList;
 
 namespace HomeSite.Controllers
 {
     public class CattleController : Controller
     {
-        // GET: Cattle
-        public ActionResult Index()
+        private IHomeSiteDb _db;
+
+        public CattleController()
         {
-            return View();
+            _db = new HomeSiteDb();
+        }
+
+        public CattleController(IHomeSiteDb db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Autocomplete(string term)
+        {
+            var model =
+                _db.Query<Cow>()
+                   .Where(r => r.Name.StartsWith(term))
+                   .Take(10)
+                   .Select(r => new
+                   {
+                       label = r.Name
+                   });
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Cattle
+        public ActionResult Index(string searchTerm = null, int page = 1)
+        {
+            List<CattleListViewModel> listBulls = new List<CattleListViewModel>();
+            List<CattleListViewModel> listCows = new List<CattleListViewModel>();
+
+            listBulls =
+                _db.Query<Bull>()
+                    .Where(r => searchTerm == null || r.Name.Contains(searchTerm))
+                    .Select(r => new CattleListViewModel
+                    {
+                        Id = r.Id,
+                        Name = r.Name,
+                        TagNumber = string.Empty,
+                        Sex = "Male"
+                    }).ToList();
+
+            listCows =
+                _db.Query<Cow>()
+                    .OrderByDescending(r => r.tagNumber)
+                    .Where(r => searchTerm == null || r.tagNumber.ToString().Contains(searchTerm) || r.Name.Contains(searchTerm))
+                    .Select(r => new CattleListViewModel
+                    {
+                        Id = r.Id,
+                        Name = r.Name,
+                        TagNumber = r.tagNumber.ToString(),
+                        Sex = "Female"
+                    }).ToList();
+
+            var model = listBulls.Union(listCows).ToPagedList(page, 10);
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Cattle", model);
+            }
+
+            return View(model);
+
+            //return View();
         }
 
         public ActionResult Cows()
@@ -22,6 +85,12 @@ namespace HomeSite.Controllers
         public ActionResult Bulls()
         {
             return View("Bulls");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
---
+++ HomeSite/HomeSite/Models/CattleListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeSite.Models
+{
+    public class CattleListViewModel
+    {
+        public int Id { get; set; }
+        public string TagNumber { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+    }
+}
